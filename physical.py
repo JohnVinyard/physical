53afee8f71436fa4b21d41a08dbe0c88018b6ace
John Vinyard <johnwho@gmail.com>
2024-07-02 17:03:10 -0500

    Write a new implementation in numpy, and optimized using numba, now that I kind of understand what's going on

--- physical.py
+++ physical.py
@@ -5,9 +5,14 @@ from io import BytesIO
 from matplotlib import pyplot as plt
 from subprocess import Popen, PIPE
 from scipy.signal import stft
+from time import time
+from numba import jit
 
 # TODO: It might be nice to move this into zounds
-def listen_to_sound(samples: np.ndarray, wait_for_user_input: bool = True) -> None:
+def listen_to_sound(
+        samples: np.ndarray, 
+        wait_for_user_input: bool = True) -> None:
+    
     bio = BytesIO()
     with SoundFile(bio, mode='w', samplerate=22050, channels=1, format='WAV', subtype='PCM_16') as sf:
         sf.write(samples.astype(np.float32))
@@ -24,142 +29,12 @@ def listen_to_sound(samples: np.ndarray, wait_for_user_input: bool = True) -> No
     
     if wait_for_user_input:
         input('Next')
+    
 
-
-class Node(object):
-    
-    home: np.ndarray
-    pos: np.ndarray
-    mass: float
-    tension: float
-    damping: float
-    velocity: np.ndarray
-    _acceleration: np.ndarray
-    constrained: bool
-    neighbors: List['Node']
-    
-    def __init__(
-            self, 
-            home: np.ndarray, 
-            mass: float, 
-            tension: float, 
-            damping: float,
-            constrained: bool = False,
-            neighbors: List['Node'] = []):
-        
-        super().__init__()
-        self.home = home
-        self.pos = np.copy(home)
-        self.mass = mass
-        self.tension = tension
-        self.damping = damping
-        self.velocity = np.zeros(home.shape)
-        self._acceleration = np.zeros(self.velocity.shape)
-        self.constrained = constrained
-        self.neighbors = neighbors
-    
-    @property
-    def force(self):
-        return self.mass * np.linalg.norm(self._acceleration)
-    
-    def _compute_home_position(self):
-        if self.neighbors:
-            s = np.stack([n.pos for n in self.neighbors])
-            home = np.mean(s, axis=0)
-            return home
-        
-        return self.home
-    
-    def apply_force(self, force: np.ndarray):
-        self.velocity += force
-    
-    def update(self, iteration: int = 0):
-        # compute the direction in which this node will be pulled
-        # by gravity or its resting point
-        diff = self._compute_home_position() - self.pos
-        direction = diff
-        
-        # acceleration is a function of tension and mass
-        acceleration = (self.tension * direction) / self.mass
-        self._acceleration = acceleration
-        
-        # acceleration modifies velocity
-        self.velocity += acceleration
-        
-        # drag reduces velocity
-        self.velocity *= self.damping
-
-    def step(self, iteration: int = 0):
-        
-        if self.constrained:
-            # this node is held in place   
-            self.pos = self.pos
-            return
-        
-        # velocity modifies position
-        self.pos += self.velocity
-
-
-def plate(
-        width: int=8, 
-        mass: float=1, 
-        tension: float=0.01, 
-        damping: float=0.999, 
-        target_node_index: List[int] = [0, 0], 
-        starting_displacement: np.ndarray = np.ones((3,))):
-    
-    print('Creating nodes')
-    nodes = [
-        [Node(
-            np.array([0, i, j], dtype=np.float32), 
-            mass=mass, 
-            tension=tension, 
-            damping=damping
-        ) for j in range(width)] for i in range(width)]
-    
-    print('connecting network')
-    for i in range(1, width - 1):
-        for j in range(1, width - 1):
-            n = nodes[i][j]
-            n.neighbors = [
-                nodes[i - 1][j - 1], 
-                nodes[i][j - 1], 
-                nodes[i + 1][j - 1], 
-                nodes[i + 1][j], 
-                nodes[i + 1][j + 1], 
-                nodes[i][j + 1], 
-                nodes[i - 1][j + 1], 
-                nodes[i - 1][j], 
-            ]
-    
-    # print('constrain ends')
-    # for i in range(width):
-    #     for j in range(width):
-    #         if i == 0 or i == width - 1 or j == 0 or j == width - 1:
-    #             nodes[i][j].constrained = True
-    
-    target_node = nodes[target_node_index[0]][target_node_index[1]]
-    
-    n_samples = 2 ** 15
-    recording = np.zeros((n_samples,))
-    
-    for s in range(n_samples):
-        
-        for i in range(width):
-            for j in range(width):
-                node = nodes[i][j]
-                if node == target_node and s == 1024:
-                    target_node.apply_force(starting_displacement)
-                node.update(s)
-        
-        for i in range(width):
-            for j in range(width):
-                node = nodes[i][j]
-                node.step()
-        
-        recording[s] = target_node.pos[0]
-        
-    
+def evaluate(recording: np.ndarray):
+    """
+    Look at time and frequency domain, then listen
+    """
     plt.plot(recording[:])
     plt.show()
     
@@ -169,88 +44,133 @@ def plate(
     
     recording = recording / (recording.max() + 1e-3)
     listen_to_sound(recording, True)
-        
-        
-        
-def multiple_nodes(
-        n_nodes: int=10, 
-        mass: float=1, 
-        tension: float=0.01, 
-        damping: float=0.999,
-        target_node_index: int=5,
-        starting_displacement=np.ones((3,))):
 
-    print(f'creating {n_nodes} nodes')
+ 
+@jit(nopython=True, nogil=True)
+def physical_modelling(
+        positions: np.ndarray, 
+        mass: float, 
+        constrained_mask: np.ndarray,
+        connectivity: np.ndarray,
+        tension: float, 
+        damping: float,
+        n_samples: int,
+        recording_target_index: int,
+        force_application_time: int,
+        force_applied: np.ndarray):
     
-    nodes = [
-        Node(
-            np.array([0, 0, i], dtype=np.float32), 
-            mass=mass, 
-            tension=tension, 
-            damping=damping) 
-        for i in np.linspace(0, 1, n_nodes)
-    ]
+    """_summary_
+
+    Args:
+        positions (np.ndarray): (n_nodes, 3) array, representing start positions
+        mass (float): scalar representing node masses
+        accelerations (np.ndarray): (n_nodes, 3) array, representing node accelerations
+        constrained_mask (np.ndarray): (n_nodes) boolean array, representing nodes that are fixed
+        connectivity (np.ndarray): (n_nodes, n_nodes) binary mask matrix, representing connectivty between nodes
+        tension (float): scalar representing mesh tension
+        damping (float): scalar representing damping/friction
+        force_application_time (int): sample index when force is applied
+        force_applied (np.ndarray): (n_nodes, 3) array representing force applied to network 
+    """
     
-    print(f'connecting network')
+    node_count: int = positions.shape[0]
+    dim: int = positions.shape[1]
     
-    # connect the network
-    for i in range(1, n_nodes - 1):
-        target = nodes[i]
-        target.neighbors = [nodes[i - 1], nodes[i + 1]]
-        print(f'setting neighbors for node {i}')
-        
-    print(f'constrain ends')
-    # fix the ends
-    nodes[0].constrained = True
-    nodes[-1].constrained = True
+    acceleration = np.zeros((node_count, dim), dtype=np.float32)
+    velocity = np.zeros((node_count, dim), dtype=np.float32)
+    recording = np.zeros(n_samples, dtype=np.float32)
+    constrained_mask = constrained_mask.reshape((node_count, 1))
     
-    
-    print(f'snap all nodes to new homes')
-    target_node = nodes[target_node_index]
-    
-    
-    n_samples = 2**15
-    
-    recording = np.zeros((n_samples,))
+    # get the number of neighbors for each node to speed up averages in the loop
+    neighbor_count = connectivity.sum(axis=-1).reshape((node_count, 1))
     
     for i in range(n_samples):
-        for node in nodes:
-            if i == 1024 and node == target_node:
-                target_node.apply_force(starting_displacement)
-            node.update(i)
-            
-        for node in nodes:
-            node.step(i)
         
-        recording[i] = target_node.pos[0]
-    
-    plt.plot(recording[:])
-    plt.show()
-    
-    _, _, spec = stft(recording, 1, window='hann')
-    plt.matshow(np.flipud(np.abs(spec.astype(np.float32))))
-    plt.show()
-    
-    recording = recording / (recording.max() + 1e-3)
-    listen_to_sound(recording, True)
-    
+        # update forces first
+        
+        if i == force_application_time:
+            velocity += force_applied
+        
+        # compute target/home position based averaged current 
+        # positions of neighbors
+        # KLUDGE: This assumes that all neighbors are equidistant and
+        # all tensions are equal
+        home_position = (connectivity @ positions) / (neighbor_count + 1e-12)
+        direction = home_position - positions
+        acceleration = (tension * direction) / mass
+        velocity += acceleration
+        velocity *= damping
 
+        # update positions
+        positions += velocity * constrained_mask
+        
+        # record from some scalar value of the model
+        recording[i] = positions[recording_target_index, 0]
+    
+    return recording
+
+def plate_simulation():
+    # TODO: This should be very similar to the string simulation, but with a
+    # different adjacency matrix
+    pass
+
+def string_simulation():
+    n_nodes, dim = 64, 3
+    
+    # nodes are positioned in a line along a single dimension
+    positions = np.zeros((n_nodes, dim))
+    positions[:, 2] = np.linspace(0, n_nodes, n_nodes)
+    
+    mass = 1
+    tension = 1
+    damping = 0.9998
+    n_samples = 2**15
+    
+    n_seconds = n_samples / 22050
+    
+    # nodes with a zero value won't have position updates
+    constrained = np.ones((n_nodes,))
+    constrained[[0, -1]] = 0
+    
+    # create the adjacency matrix.  Since this is a string, each
+    # node is connected to the node directly above and below it
+    diag = np.ones((n_nodes - 1,))
+    adjacency = np.diag(diag, 1) + np.diag(diag, -1)
+    # this isn't strictly necessary, but since the first and last node
+    # are constrained, we treat them as if they have no neighbors (they
+    # are the neighbors of other nodes, however)
+    adjacency[0, :] = 0
+    adjacency[-1, :] = 0
+    
+    force_applied = np.zeros((n_nodes, 3))
+    force_applied[33, :] = np.array([1, 10, 1])
+    
+    start = time()
+    
+    recording = physical_modelling(
+        positions=positions,
+        mass=mass,
+        constrained_mask=constrained,
+        connectivity=adjacency,
+        tension=tension,
+        damping=damping,
+        n_samples=n_samples,
+        recording_target_index=16,
+        force_application_time=1024,
+        force_applied=force_applied)
+    end = time()
+    
+    print(f'Generated {n_seconds} seconds of audio in {end - start} seconds')
+    evaluate(recording)
+    
+    
+    
     
 if __name__ == '__main__':
-    # multiple_nodes(
-    #     n_nodes=8, 
-    #     mass=16, 
-    #     tension=1, 
-    #     damping=0.999, 
-    #     target_node_index=4, 
-    #     starting_displacement=np.array([10, 10, 10]))
     
-    plate(
-        width=8, 
-        mass=16, 
-        tension=0.5, 
-        damping=0.9998, 
-        target_node_index=[1, 3], 
-        starting_displacement=np.array([1, 1, 0]))
-        
+    # Using numba's @jit is slower the first time, but
+    # more than twice as fast on subsequent runs
+    string_simulation()
+    string_simulation()
+    string_simulation()
     
